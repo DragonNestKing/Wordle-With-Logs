594c5e05783769a9b5289d5488181b31c4d71850
DragonNestKing <116030215+DragonNestKing@users.noreply.github.com>
2022-12-09 18:43:52 +0000

    The user can now register and log in on the account they registered, they can also select the no option which broke somehow, but is now fixed. The user can now only enter 5 letter words that are in the words file as well. Now onto the big issue, the data bug...

--- main.py
+++ main.py
@@ -11,37 +11,40 @@ from colorama import Fore, Back, Style
 
 not_logged = True
 
-with open("masterfile.txt", "r") as mf:
-    divided_file = mf.read().split('\n')
-    for i in range(len(divided_file)):
-        divided_file[i] = str(divided_file[i]).split(', ')
+
 
 def login(divided_file):
+    
     while True:
+        
         usern = input("Please enter your username:  ")
         passw = input("Please enter your password:  ")
 
-        with open("masterfile.txt", "r") as mf:
-            pass
-
         for i in range(len(divided_file)):
+            
             if usern == divided_file[i][0] and passw == divided_file[i][1]:
-                print("Wow, you exist")
+                
+                print("User found")
+                sleep(2)
+                os.system("clear")
                 return i
-        break
 
 
 def register():
     print("Welcome to the Wordle family!")
     while True:
+        
         while True:
+            
             usern = input("Please enter your username:  ")
             ans = input("Are you sure you want " + usern + " to be your username? [y]es or [n]o?:  ")
     
             if ans[0].lower() == "y":
                 break
+                
             elif ans[0].lower() == "n":
                 continue
+                
             else:
                 print("please enter a yes or a no.")
 
@@ -51,85 +54,117 @@ def register():
 
             if ans[0].lower() == "y":
                 break
+                
             elif ans[0].lower() == "n":
                 continue
+                
             else:
                 print("please enter a yes or a no.")
 
         user_list = ", ".join([usern, passw], )
+        
         with open("masterfile.txt", "a") as mf:
+            
             mf.write(user_list + "\n")
         os.system('clear')
         return
     
 def menu():
-    global not_logged
+    global not_logged, account_num
+            
     if not_logged:
+        
         while True:
+            with open("masterfile.txt", "r") as mf:
+                divided_file = mf.read().split('\n')
+        
+                for i in range(len(divided_file)):
+                    divided_file[i] = str(divided_file[i]).split(', ')
+                    
             ans = input("Would you like to log in to score your data? [y]es, [n]o?, or do you need to [s]ign up:  ")
+            
             if ans[0].lower() == "y":
                 os.system("clear")
                 account_num = login(divided_file)
-                break
+                not_logged = False
+                print("Let's play Wordle:\nType a 5 letter word and hit enter!\n")
+                return account_num + 1
+                
             elif ans[0].lower() == "s":
                 register()
-                print("Thank you for registering with us! Please use the login feature to login to your account.\n\n")
+                print("Thank you for registering with us!\n\n")
                 sleep(3)
+                continue
+                
             elif ans[0].lower() == "n":
                 os.system("clear")
                 break
+                
             else:
                 print("please enter yes or no")
     
     print("Let's play Wordle:\nType a 5 letter word and hit enter!\n")
-    return account_num + 1
+    return
 
 def read_random_word():
+    
     with open("words.txt") as w:
         words = w.read().splitlines()
+        
         return r.choice(words)
 
 def on_entered_line():
+    
     sys.stdout.write('\x1b[1A')
     sys.stdout.write('\x1b[2K')
 
 play_again = ""
 streak = 0
-account_num = menu()
 
 while play_again != "q":
     
+    account_num = menu()
     word = read_random_word()
     guess_list = []
 
     # need to fix this cause tutorial is strange and allows user to enter too big or too small words
     for attempt in range(1, 7):
+        
         file = open("words.txt")
         data = file.read()
         
         while True:
+            
             guess = input(Style.RESET_ALL).lower()
             on_entered_line()
             
-            if guess in data:
+            if guess in data and len(guess) == 5:
                 break
+
+            elif len(guess) != 5:
+                print("Please type a 5 letter word.")
+                
             else: 
                 print("Sorry! we don't have that word...")
-    
-        for i in range( min(len(guess), 5) ):
+
+                
+        for i in range( min(len(guess), 5)):
+                    
             if guess[i] == word[i]:
                 print(Back.GREEN + guess[i], end = "")
+                        
             elif guess[i] in word:
                 print(Back.YELLOW + guess[i], end = "")
+                        
             else:
                 print(Back.BLACK + guess[i], end = "") 
+                        
         guess_list.append(guess)
-    
+            
         print()
         if guess == word:
             print(Style.RESET_ALL + Fore.GREEN + "Congrats! You got the wordle in " + str(attempt) + " attempt(s)!")
             streak = streak + 1
-            
             break
             
     if guess != word:
@@ -138,19 +173,36 @@ while play_again != "q":
 
     data = [guess_list, word, streak]
 
-    file = open("masterfile.txt", "r+")
+    """
+    This whole section of code needs serious revising and might even need to be completely redone. as of now it produces this:
+    
+    Truth, 1234
+    Needs, 5678
+    Friend, 9999
+    ['Truth, 1234', 'Needs, 5678', 'Friend, 9999', [['fires', 'fires', 'fires', 'fires', 'fires', 'fires'], 'eaten', 0]] 
+    
+    when we need this:
+    
+    Truth, 1234, ['fires', 'fires', 'fires', 'fires', 'fires', 'fires'], 'eaten', 0
+    Needs, 5678
+    Friend, 9999
+    
+    """
+    
+    # file = open("masterfile.txt", "r+")
 
-    line_list = []
+    # line_list = []
         
-    for line in file:
-        stripped_line = line.strip()
-        line_list.append(stripped_line)
+    # for line in file:
+    #     stripped_line = line.strip()
+    #     line_list.append(stripped_line)
 
-    line_list.append(data)
+    # line_list.append(data)
 
-    file.write(str(line_list))
+    # file.write(str(line_list))
 
-    file.close()
-            
+    # file.close()   
         
     play_again = input(Style.RESET_ALL + "\nenter q if you would like to quit, press enter to play again")
+    
+    os.system("clear")
